710b8fdad9629370a5d7fb81a6711a215eb9fc83
Rohith-Bollam <bollamrohith4@gmail.com>
2025-06-18 20:10:01 +0530

    Add files via upload

---
+++ check_a_no_+ve_or_-ve.py
@@ -0,0 +1,9 @@
+a=int(input('enter the value'))
+if a>0:
+    print("its a positive no")
+elif a<0:
+    print("its a negative no")
+elif a==0:
+    print("its zero")
+else:
+    print("it not an integer")
