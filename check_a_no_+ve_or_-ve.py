e759204f5886ff2a838abd9729fb83a4208e9535
Rohith-Bollam <bollamrohith4@gmail.com>
2025-06-18 20:18:19 +0530

    Delete check_a_no_+ve_or_-ve.py

--- check_a_no_+ve_or_-ve.py
+++
@@ -1,9 +0,0 @@
-a=int(input('enter the value'))
-if a>0:
-    print("its a positive no")
-elif a<0:
-    print("its a negative no")
-elif a==0:
-    print("its zero")
-else:
-    print("it not an integer")
